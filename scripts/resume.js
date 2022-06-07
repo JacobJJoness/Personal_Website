627949a5c82f50090c3e36c5063e7dbffcddd90d
JacobJJoness <genjacob20@gmail.com>
2022-06-07 15:21:49 -0500

    Finalized Resume Page
    Resume page is now good enough for display.

---
+++ scripts/resume.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+//rotating Icon code
+
+//python = document.getElementsByClassName("icon-image-container")
+
+//python.forEach(delay());
+
+
+
+//for(let i =0; i < 80; i++){
+
+    //python.addEventListenner('animationstart', setTimeout(() => {console.log("")}))
+    
+//}
+
+
