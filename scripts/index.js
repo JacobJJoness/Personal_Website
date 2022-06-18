4392d13cefcdccfb246abbb8850918251334a7ad
JacobJJoness <genjacob20@gmail.com>
2022-06-17 20:59:12 -0500

    Attempting to Solve Page Error
    Updated js file, but once uploaded to github code does not run correctly. Attempting to resolve

--- scripts/index.js
+++ scripts/index.js
@@ -49,10 +49,10 @@ function switchQoute(i){
 
 //image carosel script for about me section
 
-let image = document.getElementById("image-carosel")
-let text = document.getElementById("carosel-words")
+let image = document.getElementById("image-carosel");
+let text = document.getElementById("carosel-words");
 let firstImages =["images/headshot.png","images/one.jpeg","images/two.jpeg","images/three.jpeg"];
-let firstWords =["Headshot",  "43 inch Black Drum", "47 inch Spinner Shark", "A piece of the Berlin wall"]
+let firstWords =["Headshot",  "43 inch Black Drum", "47 inch Spinner Shark", "A piece of the Berlin wall"];
 
 for(let i =0; i < 100; i++){
 
@@ -60,11 +60,11 @@ for(let i =0; i < 100; i++){
     
 }
 //image carosel script for fun facts page
-let funImage = document.getElementById("fun-image")
-let funText = document.getElementById("fun-carosel")
+let funImage = document.getElementById("fun-image");
+let funText = document.getElementById("fun-carosel");
 
 let funImages =["images/image0.jpeg","images/image1.jpg","images/image2.jpeg","images/image3.jpeg"];
-let funWords =["My Art",  "My Digital Art", "The Computer I built", "My Steak n' Potatoes"]
+let funWords =["My Art",  "My Digital Art", "The Computer I built", "My Steak n' Potatoes"];
 
 for(let i =0; i < 100; i++){
 
